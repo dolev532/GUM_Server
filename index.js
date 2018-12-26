331a10d9dde75a23d0f60e76d239a81d06aa824b
dolev532 <46111327+dolev532@users.noreply.github.com>
2018-12-26 12:10:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-//var express = require('express');
+var express = require('express');
 var edge = require('edge');
 
 //var connectionStr = "Data Source=DOLEV_MAIN\\SQLEXPRESS;Database=GUM;Integrated Security=True";
