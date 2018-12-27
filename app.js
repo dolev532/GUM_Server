b7d76cd964156942e67bd57ef2594f924bed2a73
dolev532 <46111327+dolev532@users.noreply.github.com>
2018-12-27 23:05:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 var express = require('../../node_modules/express');
 var app = express();
 //var edge = require('edge-js');
-//var edge = require(''../../node_modules/edge'');
+var edge = require('../../node_modules/edge'');
 
 var server = http.createServer(function(request, response) {
 
